e9e0ecc096407e369b5246c087828966be34cc3c
Saurabh <saurabh.verma@indrones.com>
2024-10-01 09:55:18 +0530

    login page, About,search,Singup Restaurant  page

--- foodhunting/src/component/Home/Home.jsx
+++ foodhunting/src/component/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../Header/Index'
 import Footer from '../Footer/Footer'
 import ResturantList from '../Resturant/ResturantList'
+import Search from '../../pages/Search'
 const Home = () => {
   return (
     <div>
       <div  className=' flex flex-col'>
       <Header/>
-      hello
+      <Search/>
       <ResturantList/>
       <Footer/>
       </div>
